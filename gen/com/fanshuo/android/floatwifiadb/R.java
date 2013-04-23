7c536fbafa411e0bfad2019906dc3d65604bfd97
fanshuo <371208484@qq.com>
2013-04-23 14:57:32 +0800

    调整浮动button
    现在长按是才可以移动，点击是开关。

--- gen/com/fanshuo/android/floatwifiadb/R.java
+++ gen/com/fanshuo/android/floatwifiadb/R.java
@@ -11,7 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg_float_button=0x7f020000;
+        public static final int bg_float_button_press=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int selector_float_button=0x7f020003;
     }
     public static final class id {
         public static final int float_button=0x7f070000;
--- src/com/fanshuo/android/floatwifiadb/MainActivity.java
+++ src/com/fanshuo/android/floatwifiadb/MainActivity.java
@@ -2,6 +2,7 @@ package com.fanshuo.android.floatwifiadb;
 
 import android.app.Activity;
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -9,15 +10,18 @@ import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	boolean hasFloatView = false;
 	boolean isOn = false;
+	boolean canMove = false;
 	private WindowManager wm = null;
 	private WindowManager.LayoutParams wmParams = null;
 
@@ -38,22 +42,6 @@ public class MainActivity extends Activity {
 	private void createView() {
 		myFV = LayoutInflater.from(this).inflate(R.layout.view_float, null);
 		floatButton = (Button) myFV.findViewById(R.id.float_button);
-		floatButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if(isOn){
-					//TODO 关闭
-					CommandUtil.RootCommand("setprop service.adb.tcp.port -1" + "\n" + "stop adbd" + "\n" + "start adbd");
-					floatButton.setText("已关闭");
-					isOn = false;
-				}else{
-					//TODO 开启
-					CommandUtil.RootCommand("setprop service.adb.tcp.port 5555" + "\n" + "stop adbd" + "\n" + "start adbd");
-					floatButton.setText("已开启");
-					isOn = true;
-				}
-			}
-		});
 		// 获取WindowManager
 		wm = (WindowManager) getApplicationContext().getSystemService("window");
 		// 设置LayoutParams(全局变量）相关参数
@@ -62,13 +50,12 @@ public class MainActivity extends Activity {
 		/**
 		 * 以下都是WindowManager.LayoutParams的相关属性 具体用途可参考SDK文档
 		 */
-		wmParams.type = LayoutParams.TYPE_PHONE; // 设置window type
+		wmParams.type = LayoutParams.TYPE_SYSTEM_ERROR; // 设置window type
 		wmParams.format = PixelFormat.RGBA_8888; // 设置图片格式，效果为背景透明
 
-		 //设置Window flag
-        wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL
-                              | LayoutParams.FLAG_NOT_FOCUSABLE;
-		
+		// 设置Window flag
+		wmParams.flags = 327976;
+
 		/*
 		 * 下面的flags属性的效果形同“锁定”。 悬浮窗不可触摸，不接受任何事件,同时不影响后面的事件响应。
 		 * wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL |
@@ -78,38 +65,75 @@ public class MainActivity extends Activity {
 		wmParams.gravity = Gravity.LEFT | Gravity.TOP; // 调整悬浮窗口至左上角
 		// 以屏幕左上角为原点，设置x、y初始值
 		wmParams.x = 0;
-		wmParams.y = 0;
+		wmParams.y = 45;
 
-		wmParams.width = ScreenSizeUtil.Dp2Px(this, 60);
-		wmParams.height = ScreenSizeUtil.Dp2Px(this, 20);
+		wmParams.width = ScreenSizeUtil.Dp2Px(this, 90);
+		wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
 
-		myFV.setOnTouchListener(new OnTouchListener() {
+		floatButton.setOnLongClickListener(new OnLongClickListener() {
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				// 获取相对屏幕的坐标，即以屏幕左上角为原点
-				x = event.getRawX();
-				y = event.getRawY() - 25; // 25是系统状态栏的高度
-				switch (event.getAction()) {
-				case MotionEvent.ACTION_DOWN:
-					// 获取相对View的坐标，即以此View左上角为原点
-					mTouchStartX = event.getX();
-					mTouchStartY = event.getY();
-					break;
-				case MotionEvent.ACTION_MOVE:
-					updateViewPosition();
-					break;
-				case MotionEvent.ACTION_UP:
-					updateViewPosition();
-					mTouchStartX = mTouchStartY = 0;
-					break;
-				}
-				return true;
+			public boolean onLongClick(View v) {
+				canMove = true;
+				return false;
 			}
 		});
-
+		floatButton.setOnTouchListener(new MyOnTouchListener());
+		floatButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onButtonClick();
+			}
+		});
 		// 显示myFloatView图像
 		wm.addView(myFV, wmParams);
+	}
 
+	class MyOnTouchListener implements OnTouchListener {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// 获取相对屏幕的坐标，即以屏幕左上角为原点
+			x = event.getRawX();
+			y = event.getRawY();
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				// 获取相对View的坐标，即以此View左上角为原点
+				mTouchStartX = event.getX();
+				mTouchStartY = event.getY();
+				break;
+			case MotionEvent.ACTION_MOVE:
+				if(canMove){
+					updateViewPosition();
+				}
+				break;
+			case MotionEvent.ACTION_UP:
+				if(canMove){
+					updateViewPosition();
+				}
+				mTouchStartX = mTouchStartY = 0;
+				break;
+			}
+			return false;
+		}
+	}
+
+	private void onButtonClick() {
+		if(canMove){
+			canMove = false;
+		}else{
+			if (isOn) {
+				// TODO 关闭
+				CommandUtil.RootCommand("setprop service.adb.tcp.port -1" + "\n"
+						+ "stop adbd" + "\n" + "start adbd");
+				floatButton.setText("Wifi调试已关闭");
+				isOn = false;
+			} else {
+				// TODO 开启
+				CommandUtil.RootCommand("setprop service.adb.tcp.port 5555" + "\n"
+						+ "stop adbd" + "\n" + "start adbd");
+				floatButton.setText("Wifi调试已开启");
+				isOn = true;
+			}
+		}
 	}
 
 	private void updateViewPosition() {
